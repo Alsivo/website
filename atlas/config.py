26bd11670b915608f4efdf825c2a5342fa46c475
datacontentengine <datacontentengine@gmail.com>
2026-08-02 23:43:33 +0900

    Update config.py

--- atlas/config.py
+++ atlas/config.py
@@ -87,5 +87,5 @@ IMAGE_OUTPUT_FORMAT = "webp"
 # Git publishing settings
 # ============================================================
 
-AUTO_GIT_PUSH = False
+AUTO_GIT_PUSH = True
 GIT_REMOTE = "origin"
