b4074eddde6a38ae980b877e061642b6bc1a1852
datacontentengine <datacontentengine@gmail.com>
2026-08-11 18:13:44 +0900

    Refine article layout and remove hero image

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -155,16 +155,6 @@ export default async function BlogPostPage({
 
         <p className="article-lead">{post.description}</p>
 
-        <div className="article-hero-image">
-          <Image
-            src={post.image}
-            alt={`${post.title}のアイキャッチ画像`}
-            fill
-            priority
-            sizes="(max-width: 820px) 100vw, 780px"
-          />
-        </div>
-
         <div className="article-meta-row">
           <time dateTime={post.date}>{publishedDate}</time>
           <span>{post.readingTime}</span>
