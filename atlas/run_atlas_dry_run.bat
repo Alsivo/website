bad59e4b2a43e85e0a435bae39c615a8b50d32e3
datacontentengine <datacontentengine@gmail.com>
2026-08-12 13:08:49 +0900

    Add Atlas scheduled run scripts

--- atlas/run_atlas_dry_run.bat
+++ atlas/run_atlas_dry_run.bat
@@ -6,6 +6,6 @@ cd /d C:\Users\hkimu\Documents\GitHub\website\atlas
 set PYTHONUTF8=1
 set PYTHONIOENCODING=utf-8
 
-"C:\Users\hkimu\Documents\GitHub\website\atlas\.venv\Scripts\python.exe" atlas.py
+"C:\Users\hkimu\Documents\GitHub\website\atlas\.venv\Scripts\python.exe" atlas.py --dry-run
 
 exit /b %ERRORLEVEL%
